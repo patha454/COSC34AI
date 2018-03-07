b6bc9d10fb6288d67b026a3626d49982d992c0ce
patha454 <patha454@student.otago.ac.nz>
2018-03-07 20:15:23 +1300

    Minor format enchancements

--- src/main.py
+++ src/main.py
@@ -20,7 +20,6 @@ import phase_two
 import phase_three
 
 
-
 """ 
 main() is the 'entry point' for the robot.
 main() will execute driveOff() [task 1], approachTower() [task 2], and
@@ -29,6 +28,8 @@ moveTower() [task 3] in sequence.
 main() assumes each of the function tasks will block until the task and the
 bot is idle, ready to begin the next phase.
 """
+
+
 def main():
     phase_one.drive_off()
     phase_two.approach_tower()
