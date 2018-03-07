7d98729654154e232494a5223fad1be3ab0c0765
patha454 <patha454@student.otago.ac.nz>
2018-03-07 20:18:36 +1300

    Added helper scripts

---
+++ src/bot.py
@@ -0,0 +1,11 @@
+"""
+bot.py
+
+bot encapsulates the motors and provides several sensor daemons to
+allow more intuitive high level use of the EV3's features
+
+Author: H Paterson
+Version: 1
+Date: 07/03/18
+"""
+
