68af1b569033384a3ebd8d269fcd99d877d2ee33
patha454 <patha454@student.otago.ac.nz>
2018-03-08 21:22:25 +1300

    Initial code to for phase one of the task.

--- src/phase_one.py
+++ src/phase_one.py
@@ -14,6 +14,15 @@ Version: 1
 """
 
 
+import bot
+from time import sleep
+import threading
+from ev3dev.ev3 import Sound
+
+
+# The number of black tiles to drive past
+TILE_DISTANCE = 15
+
 """
 drive_off() is the entry function in phaseOne.
 Only drive_off() should be used by code outside the module phaseOne.
@@ -26,7 +35,61 @@ tile is passed.
 
 
 def drive_off():
-    return
+    tile_counter = TileCounter()
+    tile_counter_thread = threading.Thread(tile_counter.count_tiles())
+    tile_counter_thread.daemon = True
+    tile_counter_thread.start()
+    bot.drive_until(lambda: tile_counter.tiles_passed >= TILE_DISTANCE)
+    bot.turn_right(bot.QUATER_TURN)
+
+
+class TileCounter:
+    # The number of black tiles passed
+    tiles_passed = 0
+
+    # The previous optical sensor reading
+    previous_colour = 0
+
+    # The current optical sensor reading
+    current_colour = 0
+
+    # The minimum increase in light intensity from a black to white tile.
+    THRESHOLD_FACTOR = 1.3
+
+    # The object to read values from
+    average_colour = None
+
+    """
+    count_tiles() - Counts the number of black tiles passed.
+    count_tiles is intended to be run in an independent thread.
+
+    count_tiles looks at the change in light intensity over time.
+    A sharp change upward must indicate we have passed from a black 
+    tile onto a white tile.
+
+    Absolute values are not used as the ambient light in the test
+    environment (Owheo lobby) will change with a number of factors
+    beyond our control.
+    """
+
+    def count_tiles(self):
+        while True:
+            if self.current_colour > self.THRESHOLD_FACTOR * self.previous_colour:
+                Sound.beep()
+                self.tiles_passed += 1
+            self.previous_colour = self.current_colour
+            self.current_colour = self.average_colour.value()
+            sleep(self.average_colour.SENSOR_PERIOD * 1.2)
+
+    """
+    Initialises the daemon thread.
+    """
+
+    def __init__(self):
+        self.average_colour = bot.LightIntensitySensor()
+        sleep(self.average_colour.SENSOR_PERIOD)
+        self.previous_colour = self.average_colour.value()
+        self.current_colour = self.previous_colour
 
 
 """
