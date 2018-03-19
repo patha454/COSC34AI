cfe65b01d2bc78351c3467778efc0026bdbf59be
patha454 <patha454@student.otago.ac.nz>
2018-03-19 13:25:03 +1300

    Phase two working isolatd

--- src/main.py
+++ src/main.py
@@ -12,7 +12,6 @@ Date: 07/03/108
 Version: 1
 """
 
-
 from ev3dev.ev3 import *
 import phase_one
 import phase_two
@@ -24,16 +23,16 @@ import phase_three
 main() is the 'entry point' for the robot.
 main() will execute driveOff() [task 1], approachTower() [task 2], and
 moveTower() [task 3] in sequence.
-
+    
 main() assumes each of the function tasks will block until the task and the
 bot is idle, ready to begin the next phase.
 """
 
 
 def main():
-    # phase_one.drive_off()
+    phase_one.drive_off()
     phase_two.approach_tower()
-    # phase_three.push_tower()
+    phase_three.push_tower()
 
 
 """
@@ -42,11 +41,12 @@ __main__() executes the bot script main(), and catches exceptions for printing
 to the console.
 """
 btn = Button()
+
 try:
     main()
 except:
     import traceback
     exc_type, exc_value, exc_traceback = sys.exc_info()
     traceback.print_exception(exc_type, exc_value, exc_traceback, file=sys.stdout)
-    while not btn.any():
+    while True:
         pass
